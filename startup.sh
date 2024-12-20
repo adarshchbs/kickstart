8f8de2dbb4bc27dd04541731399f5f3bcbbaab8e
adarshchbs <38124313+adarshchbs@users.noreply.github.com>
2024-12-20 15:36:02 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,1 +1,10 @@
-pm2 start "jupyter-lab --ip 0.0.0.0 --port 8011 --allow-root --no-browser" 
+#!/bin/bash
+
+# Start Jupyter Lab
+pm2 start "jupyter-lab --ip 0.0.0.0 --port $JUPYTER_PORT --allow-root --no-browser" --name jupyter-server
+
+# Start Code Server
+pm2 start "code-server --bind-addr 0.0.0.0:$CODE_SERVER_PORT" --name code-server
+
+# Keep container running and show logs
+pm2 logs
